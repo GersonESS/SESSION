559b475dd7e252ef0f25041d5a2728450998c136
Gerson Santos <gersones@outlook.com>
2023-11-15 15:44:52 -0300

    a

---
+++ clientes.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "<body style='blackground-color:Lightblue'>
+        <h1>Clientes!!</h1> 
+     </body>";
+
+?> 
---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+var_dump($_POST);
+if(isset($_POST['usuario'], $_POST['senha'] ))
+{
+    if(($_POST['usuario'] == 'Maria' && $_POST['senha'] == 123 ))
+    {
+        header('Location: clientes.php');
+    }
+}
+?>
+<form action="" method="post">     
+    <input type="text" name='usuario' placeholder='Usuário'>
+    <input type="text" name='senha' placeholder='Senha'>
+    <input type="submit" name='login' value 'Login'>
+</form>
