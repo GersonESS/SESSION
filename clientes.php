c04d8e5d18920b57c3fa19dcee2f66b49b39ac67
Gerson Santos <gersones@outlook.com>
2023-11-15 15:45:42 -0300

    a

--- clientes.php
+++ clientes.php
@@ -1,5 +1,9 @@
 <?php
-
+session_start();
+if(!isset($_SESSION['usuario'])){
+        header('Locatin: login.php?err-tru');
+        exit;
+}
 echo "<body style='blackground-color:Lightblue'>
         <h1>Clientes!!</h1> 
      </body>";
--- login.php
+++ login.php
@@ -1,9 +1,11 @@
 <?php
+session_start();
 var_dump($_POST);
 if(isset($_POST['usuario'], $_POST['senha'] ))
 {
     if(($_POST['usuario'] == 'Maria' && $_POST['senha'] == 123 ))
     {
+        $_SESSION['usuario'] = $_POST['usuario'];
         header('Location: clientes.php');
     }
 }
